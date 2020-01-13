f0d93be34d256f9acafe187acad427030825fbb1
tommy colella <tcolella@bsd220.org>
2020-01-13 10:29:34 -0600

    connected all my elements to storyboard

--- Favorite Cities/DetailViewController.swift
+++ Favorite Cities/DetailViewController.swift
@@ -9,32 +9,40 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configureView()
-    }
-
-    var detailItem: NSDate? {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: City? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    func configureView() {
+        // Update the user interface for the detail item
+        if let city = self.detailItem {
+            if cityTextField != nil {
+                cityTextField.text = city.name
+                stateTextField.text = city.state
+                populationTextField.text = String(city.population)
+                imageView.image = UIImage(data: city.image)
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        if let city = self.detailItem {
+           city.name = cityTextField.text!
+           city.state = stateTextField.text!
+           city.population = Int(populationTextField.text!)!
+        }
+    }
 }
 
