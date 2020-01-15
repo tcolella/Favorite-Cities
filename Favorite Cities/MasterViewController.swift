f1625a6c1efd41bbf30effafecbff8aa10efb070
tommy colella <tcolella@bsd220.org>
2020-01-15 10:49:50 -0600

    Enabled data persistence

--- Favorite Cities/MasterViewController.swift
+++ Favorite Cities/MasterViewController.swift
@@ -12,7 +12,7 @@ class MasterViewController: UITableViewController {
     
     var detailViewController: DetailViewController? = nil
     var cities = [City]()
-    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +25,18 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        if let savedData = defaults.object(forKey: "data") as? Data {
+             if let decoded = try? JSONDecoder().decode([City].self, from: savedData) {
+                   cities = decoded
+             }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
+        tableView.reloadData()
+        saveData()
     }
     
     @objc
@@ -52,27 +59,33 @@ class MasterViewController: UITableViewController {
             let stateTextField = alert.textFields![1] as UITextField
             let populationTextField = alert.textFields![2] as UITextField
             guard let image = UIImage(named: cityTextField.text!) else{
-               print("missing \(cityTextField.text!) image")
-               return }
+                print("missing \(cityTextField.text!) image")
+                return }
             if let population = Int(populationTextField.text!) {
-               let city = City(name: cityTextField.text!,
-                             state: stateTextField.text!,
-                         population: population,
-                             image: image.pngData()!)
-               self.cities.append(city)
-               self.tableView.reloadData()
+                let city = City(name: cityTextField.text!,
+                                state: stateTextField.text!,
+                                population: population,
+                                image: image.pngData()!)
+                self.cities.append(city)
+                self.tableView.reloadData()
+                self.saveData()
             }
         }
         alert.addAction(insertAction)
         present(alert, animated: true, completion: nil)
     }
     
+    func saveData() {
+        if let encoded = try? JSONEncoder().encode(cities) {
+            defaults.set(encoded, forKey: "data")
+        }
+    }
     // MARK: - Segues
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = cities[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
@@ -89,13 +102,13 @@ class MasterViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return cities.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = cities[indexPath.row]
+        cell.textLabel!.text = object.name
         return cell
     }
     
@@ -106,13 +119,18 @@ class MasterViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            cities.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
     
-    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let objectToMove = cities.remove(at: sourceIndexPath.row)
+        cities.insert(objectToMove, at: destinationIndexPath.row)
+        saveData()
+    }
 }
 
